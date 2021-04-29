971328d4ff4ff724287dd12302bb3fa32ab19cc5
daryn-99 <daryn_galeas@yahoo.com>
2021-04-28 20:07:56 -0600

    first commit

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/login_bloc.dart';
+import 'package:formvalidation/src/bloc/provider.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[
+        _crearFondo(context),
+        _loginForm(context),
+      ],
+    ));
+  }
+
+  Widget _loginForm(BuildContext context) {
+    final bloc = Provider.of(context);
+    final size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SafeArea(
+              child: Container(
+            height: 180.0,
+          )),
+          Container(
+            width: size.width * 0.85,
+            margin: EdgeInsets.symmetric(vertical: 80.0),
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3.0,
+                      offset: Offset(0.0, 5.0),
+                      spreadRadius: 3.0)
+                ]),
+            child: Column(
+              children: <Widget>[
+                //Cambiar 'Ingreso' por el nombre de la app
+                Text('Ingreso', style: TextStyle(fontSize: 20.0)),
+                SizedBox(height: 60.0),
+                _crearEmail(bloc),
+                SizedBox(height: 30.0),
+                _crearPassword(bloc),
+                SizedBox(height: 30.0),
+                _crearBoton(),
+              ],
+            ),
+          ),
+          //Agregar la pestaña de preferencia de idioma arriba
+          //Agregar ¿No tienes una cuenta? Registrate
+          Text('¿Olvido la contraseña?'),
+          SizedBox(
+            height: 50.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearEmail(LoginBloc bloc) {
+    StreamBuilder(
+        stream: bloc.emailStream,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+                  hintText: 'ejemplo@correo.com',
+                  labelText: 'Correo electronico'),
+            ),
+            onChanged: bloc.changeEmail,
+          );
+        });
+  }
+
+  Widget _crearPassword() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            icon: Icon(Icons.lock_outline, color: Colors.deepPurple),
+            labelText: 'Contraseña'),
+      ),
+    );
+  }
+
+  Widget _crearBoton() {
+    // ignore: deprecated_member_use
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      elevation: 0.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: () {},
+    );
+  }
+
+  Widget _crearFondo(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    final fondoMorado = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: <Color>[
+        Color.fromRGBO(63, 63, 156, 1.0),
+        Color.fromRGBO(60, 60, 150, 1.0)
+      ])),
+    );
+
+    final circulo = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Color.fromRGBO(255, 255, 255, 0.15)),
+    );
+
+    return Stack(
+      children: <Widget>[
+        fondoMorado,
+        Positioned(top: 90.0, left: 30.0, child: circulo),
+        Positioned(top: -40.0, right: -30.0, child: circulo),
+        Positioned(top: 160.0, right: 10.0, child: circulo),
+        Positioned(bottom: -50.0, right: -10.0, child: circulo),
+        Positioned(bottom: 120.0, right: 20.0, child: circulo),
+        Positioned(bottom: -50.0, left: -20.0, child: circulo),
+        Container(
+          padding: EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: <Widget>[
+              //Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0),
+              SizedBox(height: 10.0, width: double.infinity),
+              Image(
+                image: NetworkImage(
+                    'https://recoroatan.com/wp-content/uploads/2020/08/LOGO-RECO-YOUR-ENERGY-YOUR-WORLD-200x162-1.png'),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
